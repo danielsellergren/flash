609768df757c8f4d2e8c2c3e608d4f09b1c91e8e
Daniel Sellergren <danielsellergren@gmail.com>
2016-01-26 00:45:13 -0500

    Adds Clearance

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# Use Clearance for authentication
+gem 'clearance'
+
 # Use RedCarpet for markdown processing
 gem 'redcarpet'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Configuration for Clearance
+  config.action_mailer.default_url_options = { host: 'flashcards.danielsellergren.com' }
 end
