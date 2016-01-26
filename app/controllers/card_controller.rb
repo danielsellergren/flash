60e66a83eb4d8c69fce622c81a6fbe3e05f0a516
Daniel Sellergren <danielsellergren@gmail.com>
2016-01-26 00:13:56 -0500

    Adds scaffold for Card

---
+++ app/controllers/card_controller.rb
@@ -0,0 +1,21 @@
+class CardController < ApplicationController
+  def index
+    @cards = Card.all
+  end
+
+  def show
+    
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
