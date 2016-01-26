1216f571408b1ed5da5865ed6eba767ce8ce57fb
Daniel Sellergren <danielsellergren@gmail.com>
2016-01-25 21:49:43 -0500

    Processes definitions with markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  def parse_markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :line_numbers => :table)
+    markdown.render(text)
+  end
+
 end
