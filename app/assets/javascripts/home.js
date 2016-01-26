737a376f820f07acd7b55fbdb2cb0db8a7fff2c8
Daniel Sellergren <danielsellergren@gmail.com>
2016-01-25 23:26:39 -0500

    Adds Hide and Refresh buttons

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
   var showButton = document.getElementById('show-button');
+  var hideButton = document.getElementById('hide-button');
+  var refreshButton = document.getElementById('refresh-button')
   var definition = document.getElementById('definition');
   console.log(showButton);
   showButton.addEventListener('click', function(e) {
     definition.style.display = "block";
     showButton.style.display = "none";
+    hideButton.style.display = "inline-block"
   }, false);
+  hideButton.addEventListener('click', function(e) {
+    definition.style.display = "none";
+    showButton.style.display = "inline-block";
+    hideButton.style.display = "none";
+  }, false);
+  refreshButton.addEventListener('click', function(e) {
+    document.location.reload(true);
+  });
 });
